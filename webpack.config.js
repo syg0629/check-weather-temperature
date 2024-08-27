4da10416ebaa3ad64272dd556fcd5cc957af40fe
syg0629 <syg0629@hanmail.net>
2024-08-27 23:29:09 +0900

    Chore: 배포 준비, production 빌드 최적화 및 번들 분석 도구 추가

--- webpack.config.js
+++ webpack.config.js
@@ -4,53 +4,85 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const webpack = require("webpack");
 const dotenv = require("dotenv");
+const TerserPlugin = require("terser-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 //.env.local 파일에서 환경변수 로드
 dotenv.config({ path: "./.env.local" });
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.ts",
-  devServer: {
-    static: "./dist",
-    hot: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        exclude: /node_modules/,
-        use: "ts-loader",
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "images/[name][ext]",
+module.exports = (argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    mode: isProduction ? "production" : "development",
+    entry: "./src/index.ts",
+    devtool: isProduction ? "source-map" : "eval-source-map",
+    devServer: {
+      static: "./dist",
+      hot: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          use: "ts-loader",
         },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "images/[name][ext]",
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".ts", ".js"],
+    },
+    optimization: {
+      minimize: isProduction,
+      minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+      usedExports: true,
+      sideEffects: true,
+      splitChunks: {
+        chunks: "all",
       },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        minify: isProduction,
+      }),
+      new MiniCssExtractPlugin({
+        filename: isProduction ? "[name].[contenthash].css" : "[name].css",
+      }),
+      new CopyPlugin({
+        patterns: [{ from: "assets", to: "images" }],
+      }),
+      new webpack.EnvironmentPlugin(["APP_SERVICE_KEY", "KAKAO_REST_API_KEY"]),
+      new BundleAnalyzerPlugin({
+        analyzerMode: "static",
+        openAnalyzer: false,
+      }),
+      new MiniCssExtractPlugin({
+        filename: isProduction ? "[name].[contenthash].css" : "[name].css",
+      }),
     ],
-  },
-  resolve: {
-    extensions: [".ts", ".js"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-    new MiniCssExtractPlugin(),
-    new CopyPlugin({
-      patterns: [{ from: "assets", to: "images" }],
-    }),
-    new webpack.EnvironmentPlugin(["APP_SERVICE_KEY", "KAKAO_REST_API_KEY"]),
-  ],
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    assetModuleFilename: "images/[name][ext]",
-  },
+    output: {
+      filename: isProduction ? "[name].[contenthash].js" : "[name].bundle.js",
+      chunkFilename: isProduction
+        ? "[name].[contenthash].js"
+        : "[name].chunk.js",
+      path: path.resolve(__dirname, "dist"),
+      assetModuleFilename: "images/[name][ext]",
+      clean: true,
+    },
+  };
 };
