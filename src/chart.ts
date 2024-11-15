8ed1641488af548e5c6edefa57828444040e33e0
syg0629 <syg0629@hanmail.net>
2024-11-15 23:53:21 +0900

    Refactor: 불필요한 조건문 제거 및 가독성 개선

--- src/chart.ts
+++ src/chart.ts
@@ -43,55 +43,53 @@ export const createChart = (
   if (existingChart) existingChart.destroy();
 
   // 날씨 이모지 표시(어제, 오늘 내일만)
-  if (options) {
-    const weatherEmojisContainer = canvas
-      .closest(".chart-container")
-      ?.querySelector(".weather-emojis");
+  const weatherEmojisContainer = canvas
+    .closest(".chart-container")
+    ?.querySelector(".weather-emojis");
 
-    if (weatherEmojisContainer) {
-      weatherEmojisContainer.innerHTML = labels
-        .map((label) => {
-          const [date, emoji] = label.split("\n");
-          return `<div class="weather-emoji-wrapper"><span>${date}</span><span>${
-            emoji || ""
-          }</span></div>`;
-        })
-        .join("");
-    }
+  if (weatherEmojisContainer) {
+    weatherEmojisContainer.innerHTML = labels
+      .map((label) => {
+        const [date, emoji] = label.split("\n");
+        return `<div class="weather-emoji-wrapper"><span>${date}</span><span>${
+          emoji || ""
+        }</span></div>`;
+      })
+      .join("");
+  }
 
-    // 차트 생성 및 반환
-    return new Chart(ctx, {
-      type: "line",
-      data: { labels, datasets },
-      options: {
-        animation: false,
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-          x: {
-            ticks: {
-              callback: function (value: number) {
-                // 날짜와 날씨 이모지 수동 줄바꿈 처리
-                const label = this.getLabelForValue(value);
-                return label ? label.split("\n") : label;
-              },
-              font: { size: calculateFontsSize() },
+  // 차트 생성 및 반환
+  return new Chart(ctx, {
+    type: "line",
+    data: { labels, datasets },
+    options: {
+      animation: false,
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        x: {
+          ticks: {
+            callback: function (value: number) {
+              // 날짜와 날씨 이모지 수동 줄바꿈 처리
+              const label = this.getLabelForValue(value);
+              return label ? label.split("\n") : label;
             },
-          },
-          y: {
-            ticks: {
-              font: { size: calculateFontsSize() },
-            },
+            font: { size: calculateFontsSize() },
           },
         },
-        plugins: {
-          legend: {
-            labels: {
-              font: { size: calculateFontsSize(16) },
-            },
+        y: {
+          ticks: {
+            font: { size: calculateFontsSize() },
           },
         },
       },
-    });
-  }
+      plugins: {
+        legend: {
+          labels: {
+            font: { size: calculateFontsSize(16) },
+          },
+        },
+      },
+    },
+  });
 };
