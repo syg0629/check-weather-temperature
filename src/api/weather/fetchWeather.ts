bff5d6d5862388fa7a110be1a1b7256b8cd555c2
syg0629 <syg0629@hanmail.net>
2024-11-17 20:25:31 +0900

    Feat: 어제 날씨 데이터 처리를 위한 로직 추가 및 데이터 갱신 주기에 따른 날짜 설정 로직 추가
    - 어제의 예보 데이터를 처리하기 위한 `fetchYesterdayForecast` 함수 추가
    - 날짜 설정 로직(`getYesterdayBaseTime`, `getWeeklyBaseDate`) 추가로 데이터 갱신 시간 기준 처리
    - `fetchShortTermForecast`와 `fetchWeeklyForecast`에서 데이터 검증 및 에러 메시지 개선
    - 에러 메시지 개선 및 예외 처리 강화

--- src/api/weather/fetchWeather.ts
+++ src/api/weather/fetchWeather.ts
@@ -1,37 +1,98 @@
 import { userLocation } from "../../types/type";
+import { DATES, getFormattedDate } from "../../utils/date";
 import { fetchData } from "../fetchData";
 import { fetchReverseGeo } from "../geo/kakaoReverseGeo";
 
 const APP_SERVICE_KEY = process.env.APP_SERVICE_KEY;
+const currentDate = new Date();
+const currentHour = currentDate.getHours();
 
-// 단기 예보(어제, 오늘, 내일 예보)
+//데이터 갱신 주기에 따른 시간 설정
+const YESTERDAY_DATA_UPDATE_HOUR = 11;
+const WEEKLY_DATA_UPDATE_HOUR = 6;
+
+// 어제 예보
+const getYesterdayBaseTime = (date: string) => {
+  if (currentHour < YESTERDAY_DATA_UPDATE_HOUR) {
+    return getFormattedDate(-2);
+  }
+  return DATES.yesterday;
+};
+
+export const fetchYesterdayForecast = async (
+  date: string,
+  userLocation: userLocation
+) => {
+  try {
+    date = getYesterdayBaseTime(date);
+    const branchCode = (await fetchReverseGeo(userLocation)).branchCode;
+    if (!branchCode) throw new Error("지역 코드를 가져오는데 실패했습니다.");
+
+    const url = `https://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList?serviceKey=${APP_SERVICE_KEY}&pageNo=1&numOfRows=10&dataType=JSON&dataCd=ASOS&dateCd=DAY&startDt=${date}&endDt=${date}&stnIds=${branchCode}`;
+    const data = await fetchData(url);
+
+    if (!data?.response?.body?.items?.item) {
+      console.error("API 응답: ", data);
+      throw new Error("API 응답 데이터 형식이 올바르지 않습니다.");
+    }
+
+    return data.response.body.items.item;
+  } catch (error) {
+    console.error(`어제 데이터를 가져오는 중 오류 발생: ${date}:`, error);
+    throw error;
+  }
+};
+
+// 단기 예보(오늘, 내일 예보)
 export const fetchShortTermForecast = async (
   date: string,
   userLocation: userLocation
 ) => {
   try {
+    if (!userLocation) throw new Error("지역 코드를 가져오는데 실패했습니다.");
+
     const url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${APP_SERVICE_KEY}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${date}&base_time=0200&nx=${userLocation.x}&ny=${userLocation.y}`;
     const data = await fetchData(url);
+
+    if (!data?.response?.body?.items?.item) {
+      console.error("API 응답: ", data);
+      throw new Error("API 응답 데이터 형식이 올바르지 않습니다.");
+    }
+
     return data.response.body.items.item;
   } catch (error) {
-    console.error("단기 예보 데이터를 가져오는 중 오류가 발생했습니다:", error);
-    alert("단기 예보 데이터를 가져오는 중 오류가 발생했습니다.");
+    console.error(`단기 데이터를 가져오는 중 오류 발생: ${date}:`, error);
+    throw error;
   }
 };
 
 // 주간 예보
+const getWeeklyBaseDate = (date: string) => {
+  if (currentHour < WEEKLY_DATA_UPDATE_HOUR) {
+    return DATES.yesterday;
+  }
+  return date;
+};
+
 export const fetchWeeklyForecast = async (
   date: string,
   userLocation: userLocation
 ) => {
   try {
-    const locationCode = await fetchReverseGeo(userLocation);
-    const url = `https://apis.data.go.kr/1360000/MidFcstInfoService/getMidTa?serviceKey=${APP_SERVICE_KEY}&pageNo=1&numOfRows=10&dataType=JSON&regId=${locationCode}&tmFc=${date}
-  0600`;
+    date = getWeeklyBaseDate(date);
+    const areaCode = (await fetchReverseGeo(userLocation)).areaCode;
+    if (!areaCode) throw new Error("지역 코드를 가져오는데 실패했습니다.");
+
+    const url = `https://apis.data.go.kr/1360000/MidFcstInfoService/getMidTa?serviceKey=${APP_SERVICE_KEY}&pageNo=1&numOfRows=10&dataType=JSON&regId=${areaCode}&tmFc=${date}0600`;
     const data = await fetchData(url);
+
+    if (!data?.response?.body?.items?.item?.[0]) {
+      throw new Error("API 응답 데이터 형식이 올바르지 않습니다.");
+    }
+
     return data.response.body.items.item[0];
   } catch (error) {
-    console.error("주간 예보 데이터를 가져오는 중 오류가 발생했습니다:", error);
-    alert("주간 예보 데이터를 가져오는 중 오류가 발생했습니다.");
+    console.error("주간 예보 데이터를 가져오는 중 오류 발생:", error);
+    throw error;
   }
 };
