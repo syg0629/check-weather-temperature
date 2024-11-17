2ba07c173056a890e732411aaca443d27d629f4f
syg0629 <syg0629@hanmail.net>
2024-11-17 15:37:13 +0900

    Feat: 어제 날씨를 가져오기 위해 지점 코드 처리 로직 추가
    - `fetchReverseGeo` 함수에서 지점 코드도 반환하도록 수정
    - `DEFAULT_AREA_CODE`를 `DEFAULT_LOCATION`으로 변경하고 지점코드도 함께 처리
    - `getAreaCode` 함수를 `getAreaBranchCode`로 변경하여 예보구역 코드와 지점 코드를 함께 반환하도록 수정

--- src/api/geo/kakaoReverseGeo.ts
+++ src/api/geo/kakaoReverseGeo.ts
@@ -1,14 +1,18 @@
-import { userLocation } from "../../types/type";
+import { LocationCode, userLocation } from "../../types/type";
 import { areaCodeMap } from "../../utils/areaCodeMap";
+import { branchCodeMap } from "../../utils/branchCodeMap";
 import { fetchData } from "../fetchData";
 
+// 예보구역코드, 지점코드 오류 시 서울이 기본 값
+const DEFAULT_LOCATION = {
+  areaCode: "11B10101",
+  branchCode: "108",
+};
 const KAKAO_REST_API_KEY = process.env.KAKAO_REST_API_KEY;
-// 예보구역코드 오류 시 서울이 기본 값
-const DEFAULT_AREA_CODE = "11B10101";
 
 export const fetchReverseGeo = async (
   userLocation: userLocation
-): Promise<string> => {
+): Promise<LocationCode> => {
   try {
     const url = `https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${userLocation.lng}&y=${userLocation.lat}`;
     const data = await fetchData(url, {
@@ -23,12 +27,12 @@ export const fetchReverseGeo = async (
       region_2depth_name,
       region_3depth_name
     );
-    return getAreaCode(region_1depth_name);
+    return getAreaBranchCode(region_1depth_name);
   } catch (error) {
     alert(
       "카카오 맵을 통해 예보구역코드를 가져오는 중 오류가 발생하였습니다. 기본 위치(서울)로 날씨 정보를 가져옵니다."
     );
-    return DEFAULT_AREA_CODE;
+    return DEFAULT_LOCATION;
   }
 };
 
@@ -46,12 +50,13 @@ const updateLocationDisplay = (
 };
 
 // 기상청 주간 예보 API 사용 시 필요한 예보구역코드 가져오기
-const getAreaCode = (locationName: string) => {
-  const code = areaCodeMap.get(locationName);
-  if (code) return code;
+const getAreaBranchCode = (locationName: string) => {
+  const areaCode = areaCodeMap.get(locationName);
+  const branchCode = branchCodeMap.get(locationName);
 
+  if (areaCode && branchCode) return { areaCode, branchCode };
   alert(
     `"${locationName}"의 예보구역코드를 찾을 수 없어 기본 위치(서울)로 날씨 정보를 가져옵니다.`
   );
-  return DEFAULT_AREA_CODE;
+  return DEFAULT_LOCATION;
 };
