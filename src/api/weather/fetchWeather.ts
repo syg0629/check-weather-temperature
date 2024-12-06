7c458cf7eea67a35ebca5f4d4ac19e591ab59a89
syg0629 <syg0629@hanmail.net>
2024-12-06 17:38:39 +0900

    Refactor: 단기 예보 API 시간 지정 및 코드 정리
    - 단기 예보 API 호출 시 시간대 별로 받아오는 데이터 정보가 달라 시간 지정
    - 불필요한 매개변수 제거

--- src/api/weather/fetchWeather.ts
+++ src/api/weather/fetchWeather.ts
@@ -11,20 +11,35 @@ const currentHour = currentDate.getHours();
 const YESTERDAY_DATA_UPDATE_HOUR = 11;
 const WEEKLY_DATA_UPDATE_HOUR = 6;
 
-// 어제 예보
-const getYesterdayBaseTime = (date: string) => {
+// 종간 예보(어제)
+const getYesterdayBaseTime = () => {
   if (currentHour < YESTERDAY_DATA_UPDATE_HOUR) {
     return getFormattedDate(-2);
   }
   return DATES.yesterday;
 };
 
-export const fetchYesterdayForecast = async (
-  date: string,
-  userLocation: userLocation
-) => {
+// 단기 예보(오늘, 내일, 글피, 그글피)
+const getShortTermBaseTime = () => {
+  // 0~1시는 전날 23시 데이터 사용
+  if (currentHour < 2) {
+    return {
+      date: getFormattedDate(-1),
+      time: "2300",
+    };
+  }
+
+  // 그 외 시간은 02시 데이터
+  // 시간대 마다 가져오는 정보(fcstValue 예보날짜, category)가 다르기 때문에 02시로 지정하여 사용
+  return {
+    date: getFormattedDate(0),
+    time: "0200",
+  };
+};
+
+export const fetchYesterdayForecast = async (userLocation: userLocation) => {
   try {
-    date = getYesterdayBaseTime(date);
+    const date = getYesterdayBaseTime();
     const branchCode = (await fetchReverseGeo(userLocation)).branchCode;
     if (!branchCode) throw new Error("지역 코드를 가져오는데 실패했습니다.");
 
@@ -38,20 +53,18 @@ export const fetchYesterdayForecast = async (
 
     return data.response.body.items.item;
   } catch (error) {
-    console.error(`어제 데이터를 가져오는 중 오류 발생: ${date}:`, error);
+    console.error(`어제 데이터를 가져오는 중 오류 발생: `, error);
     throw error;
   }
 };
 
 // 단기 예보(오늘, 내일 예보)
-export const fetchShortTermForecast = async (
-  date: string,
-  userLocation: userLocation
-) => {
+export const fetchShortTermForecast = async (userLocation: userLocation) => {
   try {
+    const { date, time } = getShortTermBaseTime();
     if (!userLocation) throw new Error("지역 코드를 가져오는데 실패했습니다.");
 
-    const url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${APP_SERVICE_KEY}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${date}&base_time=0200&nx=${userLocation.x}&ny=${userLocation.y}`;
+    const url = `https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey=${APP_SERVICE_KEY}&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${date}&base_time=${time}&nx=${userLocation.x}&ny=${userLocation.y}`;
     const data = await fetchData(url);
 
     if (!data?.response?.body?.items?.item) {
@@ -61,12 +74,12 @@ export const fetchShortTermForecast = async (
 
     return data.response.body.items.item;
   } catch (error) {
-    console.error(`단기 데이터를 가져오는 중 오류 발생: ${date}:`, error);
+    console.error(`단기 데이터를 가져오는 중 오류 발생: `, error);
     throw error;
   }
 };
 
-// 주간 예보
+// 중기 예보
 const getWeeklyBaseDate = (date: string) => {
   if (currentHour < WEEKLY_DATA_UPDATE_HOUR) {
     return DATES.yesterday;
@@ -85,7 +98,6 @@ export const fetchWeeklyForecast = async (
 
     const url = `https://apis.data.go.kr/1360000/MidFcstInfoService/getMidTa?serviceKey=${APP_SERVICE_KEY}&pageNo=1&numOfRows=10&dataType=JSON&regId=${areaCode}&tmFc=${date}0600`;
     const data = await fetchData(url);
-
     if (!data?.response?.body?.items?.item?.[0]) {
       throw new Error("API 응답 데이터 형식이 올바르지 않습니다.");
     }
