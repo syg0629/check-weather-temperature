72b51aabfa5a9b2e2512e8b39a7f633e79d03435
syg0629 <syg0629@hanmail.net>
2024-12-14 21:56:33 +0900

    Feat: 중기 예보 날씨 코드 이모지 매핑 추가
    - 기상청 중기 예보 API에서 제공되는 한글 날씨 데이터를 이모지로 매핑하는 weeklyWeather 및 weeklyWeatherEmojis 추가

--- src/utils/weatherEmojis.ts
+++ src/utils/weatherEmojis.ts
@@ -1,4 +1,4 @@
-// 기상청 API 단기 예보에 명시되어 있는 날씨 코드를 이용하여 날씨 이모지로 시각화
+// 기상청 단기 예보 API에 명시되어 있는 날씨 코드를 이용하여 날씨 이모지로 시각화
 export const pytCode = {
   0: "강수 없음",
   1: "비",
@@ -18,4 +18,20 @@ export const skyEmojis: Record<string, string> = {
   "1": "☀️",
   "3": "⛅️",
   "4": "☁️",
+};
+
+// 기상청 중기 예보 API는 단기 예보와 달리 한글로 데이터가 주어짐
+// 해당 단어들이 포함되어 있는 경우 이모지로 표시
+export const weeklyWeather = {
+  sun: ["맑음"],
+  cloud: ["구름많음", "흐림"],
+  rain: ["구름많고 비", "구름많고 소나기", "흐리고 비", "흐리고 소나기"],
+  snow: ["구름많고 눈", "구름많고 비/눈", "흐리고 비/눈", "흐리고 눈"],
+};
+
+export const weeklyWeatherEmojis: Record<string, string> = {
+  sun: "☀️",
+  cloud: "☁️",
+  rain: "🌧️",
+  snow: "❄️",
 };
