bfff00d3fa3196f937339c087eadd9f305d9ecc2
syg0629 <syg0629@hanmail.net>
2025-05-24 23:59:47 +0900

    Refactor: 캐시 지속 시간 변경 및 캐시 키 개선 - 캐시 키 동일하여 shormTerm과 weekly 덮어씀 - 자정이 되었을 때 새로운 캐시를 받게 설정

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,9 +1,15 @@
+import { DATES } from "../constants/date";
 import { userLocation } from "../types/type";
+import { getFormattedDate } from "./date";
 
-const CACHE_DURATION = 1000 * 60 * 60 * 24;
+type CacheType = "shortTerm" | "weekly";
 
-export const getCachedWeather = (location: userLocation) => {
-  const key = `weather_${location.x}_${location.y}`;
+// 캐시 지속 시간 30분
+const CACHE_DURATION = 30 * 60 * 1000;
+
+export const getCachedWeather = (location: userLocation, type: CacheType) => {
+  const today = getFormattedDate(0);
+  const key = `weather_${type}_${today}_${location.x}_${location.y}`;
   const cached = localStorage.getItem(key);
 
   if (!cached) return null;
@@ -18,9 +24,13 @@ export const getCachedWeather = (location: userLocation) => {
   return data;
 };
 
-
-export const setCachedWeather = (location: userLocation, data: any) => {
-  const key = `weather_${location.x}_${location.y}`;
+export const setCachedWeather = (
+  location: userLocation,
+  data: any,
+  type: CacheType
+) => {
+  const today = getFormattedDate(0);
+  const key = `weather_${type}_${today}_${location.x}_${location.y}`;
   const cacheData = {
     data,
     time: Date.now(),
